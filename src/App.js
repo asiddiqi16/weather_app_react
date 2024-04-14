122ee3e0af7a7e16e8d4807631d4cc2d8d1522e2
Ariba S <153156108+asiddiqi16@users.noreply.github.com>
2024-04-14 22:19:55 +1000

    Changed API call structure and added css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import axios from "axios";
 import "./App.css";
 import CurrentWeather from "./CurrentWeather";
 import Footer from "./Footer";
+import { ThreeDots } from "react-loader-spinner";
 
 export default function App() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
 
   function showTemp(response) {
-    console.log(response.data);
     let temp = response.data.temperature.current;
     let condition = response.data.condition.description;
     let humidity = response.data.temperature.humidity;
@@ -25,10 +25,10 @@ export default function App() {
       date: todaysDate,
     };
     setWeather(weather);
+    console.log(weather.temperature);
   }
   function showWeather(event) {
     event.preventDefault();
-
     let apiKey = "483ecb596o30da81tf76d2a4bf19d4a6";
     let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiURL).then(showTemp);
@@ -36,25 +36,60 @@ export default function App() {
   function updateCity(event) {
     setCity(event.target.value);
   }
-  return (
-    <div className="App">
-      <header className="Form">
-        <form id="search-form" onSubmit={showWeather}>
-          <input
-            type="search"
-            placeholder="Enter a city.."
-            required
-            className="SearchInput"
-            id="search-input"
-            onChange={updateCity}
-          />
-          <input type="submit" value="Search" className="SearchButton" />
-        </form>
-      </header>
-      <main>
-        <CurrentWeather cityName={city} weather={weather} />
-      </main>
-      <Footer />
-    </div>
-  );
+  if (weather != null) {
+    return (
+      <div className="App">
+        <header className="Form">
+          <form id="search-form" onSubmit={showWeather}>
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              required
+              className="SearchInput"
+              id="search-input"
+              onChange={updateCity}
+            />
+            <input type="submit" value="Search" className="SearchButton" />
+          </form>
+        </header>
+        <main>
+          <CurrentWeather cityName={city} weather={weather} />
+        </main>
+        <Footer />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <header className="Form">
+          <form id="search-form" onSubmit={showWeather}>
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              required
+              className="SearchInput"
+              id="search-input"
+              onChange={updateCity}
+            />
+            <input type="submit" value="Search" className="SearchButton" />
+          </form>
+        </header>
+        <main>
+          <div className="Loading">
+            <ThreeDots
+              visible={true}
+              height="80"
+              width="80"
+              color="#885df1"
+              radius="9"
+              ariaLabel="three-dots-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </div>
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,56 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CurrentWeather.css";
-import { ThreeDots } from "react-loader-spinner";
+
 import FormattedDate from "./FormattedDate";
 
 export default function CurrentWeather(props) {
-  if (props.weather === null) {
-    return (
-      <div className="Loading">
-        <ThreeDots
-          visible={true}
-          height="80"
-          width="80"
-          color="#885df1"
-          radius="9"
-          ariaLabel="three-dots-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-        />
-      </div>
-    );
-  } else {
-    return (
-      <div className="CurrentWeather">
-        <div>
-          <h1 className="CurrentCity" id="current-city">
-            {props.city}
-          </h1>
-          <p className="CurrentDetails">
-            <span id="current-date">
-              <FormattedDate todaysDate={props.weather.date} />
-            </span>
-            , {props.weather.desc} <br />
-            Humidity: <strong>{props.weather.humidity}%</strong>, Wind:{" "}
-            <strong>{props.weather.wind}km/h</strong>
-          </p>
-        </div>
-        <div className="CurrentTemperature">
-          <span
-            className="CurrentTemperatureIcon"
-            id="current-temperature-icon"
-          >
-            <img src={props.weather.icon} alt="weather-icon" />
-          </span>
-          <span
-            className="CurrentTemperatureValue"
-            id="CurrentTemperatureValue"
-          >
-            {Math.round(props.weather.temperature)}
-          </span>
-          <span className="CurrentTemperatureUnit">°C</span>
-        </div>
-      </div>
-    );
+  const [temperature, setTemperature] = useState(
+    Math.round(props.weather.temperature)
+  );
+  console.log(temperature);
+  const [metric, setMetric] = useState("temp-units boldtext");
+  const [imperial, setImperial] = useState("temp-units");
+  function showFaranheit(event) {
+    event.preventDefault();
+    setTemperature(Math.round(props.weather.temperature * (9 / 5) + 32));
+    setMetric("temp-units");
+    setImperial("temp-units boldtext");
   }
+  function showCelsius(event) {
+    event.preventDefault();
+    setTemperature(Math.round(props.weather.temperature));
+    setMetric("temp-units boldtext");
+    setImperial("temp-units");
+  }
+
+  return (
+    <div className="CurrentWeather">
+      <div>
+        <h1 className="CurrentCity" id="current-city">
+          {props.city}
+        </h1>
+        <p className="CurrentDetails">
+          <span id="current-date">
+            <FormattedDate todaysDate={props.weather.date} />
+          </span>
+          , {props.weather.desc} <br />
+          Humidity: <strong>{props.weather.humidity}%</strong>, Wind:{" "}
+          <strong>{props.weather.wind}km/h</strong>
+        </p>
+      </div>
+      <div className="CurrentTemperature">
+        <span className="CurrentTemperatureIcon" id="current-temperature-icon">
+          <img src={props.weather.icon} alt="weather-icon" />
+        </span>
+        <span className="CurrentTemperatureValue" id="CurrentTemperatureValue">
+          {temperature}
+        </span>
+        <span className="CurrentTemperatureUnit">
+          <a
+            href="/"
+            className={metric}
+            id="temp-units-C"
+            onClick={showCelsius}
+          >
+            °C{" "}
+          </a>
+          <span className="temp-units">|</span>
+          <a
+            href="/"
+            className={imperial}
+            id="temp-units-F"
+            onClick={showFaranheit}
+          >
+            °F{" "}
+          </a>
+        </span>
+      </div>
+    </div>
+  );
 }
